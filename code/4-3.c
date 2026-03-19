ba0cac57bc57960ca65d17b4ae265d88702522cb
kanghyein75 <kanghye675@gmail.com>
2026-03-19 22:42:03 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,6 +2,22 @@
 
 int main()
 {
+    int a;
+    scanf("%d", &a);
+    char c = 'f'
+
+    for (int i = 2; i < a; i++) {
+        if (a % i == 0) {
+            c = 't';
+            break;
+        }
+    }
+
+    if(c == 't') {
+        printf("%s", "ture");
+    } else if(c == 'f') {
+        printf("%s", "false");
+    }
 
     return 0;
 }
