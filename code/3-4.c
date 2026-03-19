588bf38d3a4b7e3747c356469069f2d34b328d48
kanghyein75 <kanghye675@gmail.com>
2026-03-19 23:01:04 +0900

    Update 3-4.c

--- code/3-4.c
+++ code/3-4.c
@@ -8,10 +8,10 @@ int main()
     printf("%d %d %d\n", a, b, c);
 
     a = 2 + 3 * 4;
-    prinf("%d %d %d\n", a, b, c);
+    printf("%d %d %d\n", a, b, c);
 
     c = a++ + ++b;
-    prinf("%d %d %d\n", a, b, c);
+    printf("%d %d %d\n", a, b, c);
 
     return 0;
 }
