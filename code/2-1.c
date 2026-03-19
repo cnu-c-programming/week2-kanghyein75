459c3f39bab0a3040fe066ee759575fcb90d0c0c
kanghyein75 <kanghye675@gmail.com>
2026-03-19 22:04:17 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -2,6 +2,13 @@
 
 int main()
 {
+    printf("char: %zu\n", sizeof(char));
+    printf("short: %zu\n", sizeof(short));
+    printf("int: %zu\n", sizeof(int));
+    printf("unsigned int: %zu\n", sizeof(unsigned int));
+    printf("long: %zu\n", sizeof(long));
+    printf("long long: %zu\n", sizeof(long long));
+    printf("float: %zu\n", sizeof(float));
 
     return 0;
 }
