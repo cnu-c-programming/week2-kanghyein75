e37e548bd6d38487be6b09285deadcd3f24f5b6f
kanghyein75 <kanghye675@gmail.com>
2026-03-19 22:41:26 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,6 +2,21 @@
 
 int main()
 {
+    int a;
+    int b;
+    char c;
+    
+    scanf("%d %d %c", &a, &b, &c);
+
+    if (c == '+') {
+        printf("%d\n", a+b);
+    } else if (c == '-') {
+        prinf("%d\n", a-b);
+    } else if (c == '*') {
+        prinf("%d\n", a*b);
+    } else if (c == '/') {
+        prinf("%d\n", a/b);
+    }
 
     return 0;
 }
